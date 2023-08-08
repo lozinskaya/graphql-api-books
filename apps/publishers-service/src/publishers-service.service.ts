e62dc5b4482c560b89d89222a688beb91c6184d7
natalalozinskaa <guvegina15N!>
2023-08-09 00:39:01 +0500

    refactor(publisher): перенести сервис из library-service в publishers-service
    МЕТА ДАННЫЕ: #5

--- apps/publishers-service/src/publishers-service.service.ts
+++ apps/publishers-service/src/publishers-service.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@nestjs/common';
 
+import { IPublisher, ICreatePublisherInput } from './publishers-service.interface';
+
 @Injectable()
 export class CPublishersServiceService {
-  getHello(): string {
-    return 'Hello World!';
+  private publishers: IPublisher[] = [
+    {
+      id: 1,
+      title: 'Эксмо',
+    },
+    {
+      id: 2,
+      title: 'АСТ',
+    },
+  ];
+
+  findAll() {
+    return this.publishers;
+  }
+
+  findOne(id: number) {
+    return this.publishers.find((publisher) => publisher.id === id);
+  }
+
+  create(createPublisherInput: ICreatePublisherInput) {
+    const newPublisher = {
+      id: this.publishers.length + 1,
+      ...createPublisherInput,
+    };
+
+    this.publishers.push(newPublisher);
+
+    return newPublisher;
   }
 }
