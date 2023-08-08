eb88fa0e04071a90377963165de8e74d732f5689
natalalozinskaa <guvegina15N!>
2023-08-08 18:20:23 +0500

    feat(author): преобразовать модуль в мироксервис
    МЕТА ДАННЫЕ: #3

--- apps/authors-service/src/main.ts
+++ apps/authors-service/src/main.ts
@@ -1,8 +1,21 @@
+import { join } from 'path';
+
 import { NestFactory } from '@nestjs/core';
-import { AuthorsServiceModule } from './authors-service.module';
+import { Transport } from '@nestjs/microservices/enums';
+
+import { CAuthorsServiceModule } from './authors-service.module';
+const microserviceOptions = {
+  transport: Transport.GRPC,
+  options: {
+    url: `0.0.0.0:3001`,
+    package: 'authors',
+    protoPath: join(process.cwd(), 'apps/authors-service/src/authors-service.proto'),
+  },
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthorsServiceModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(CAuthorsServiceModule, microserviceOptions);
+
+  await app.listen().then(() => console.log('Authors service is listening...'));
 }
 bootstrap();
