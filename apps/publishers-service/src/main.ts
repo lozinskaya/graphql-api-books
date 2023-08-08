f56bf2bfb0e271dc45154eec37979a62bacf504b
natalalozinskaa <guvegina15N!>
2023-08-09 00:34:16 +0500

    refactor(publisher): преобразовать сервис в мироксервис
    МЕТА ДАННЫЕ: #5

--- apps/publishers-service/src/main.ts
+++ apps/publishers-service/src/main.ts
@@ -1,10 +1,22 @@
+import { join } from 'path';
+
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices/enums';
 
 import { CPublishersServiceModule } from './publishers-service.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(CPublishersServiceModule);
+const microserviceOptions = {
+  transport: Transport.GRPC,
+  options: {
+    url: `0.0.0.0:3002`,
+    package: 'publishers',
+    protoPath: join(process.cwd(), 'apps/publishers-service/src/publishers-service.proto'),
+  },
+};
 
-  await app.listen(3000);
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice(CPublishersServiceModule, microserviceOptions);
+
+  await app.listen().then(() => console.log('Publishers service is listening...'));
 }
 bootstrap();
