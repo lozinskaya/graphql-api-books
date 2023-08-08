97a3414a2536e1876eaf7001f300590a5a47abeb
natalalozinskaa <guvegina15N!>
2023-08-09 00:40:48 +0500

    feat(publisher): описать методы контроллера publisher-service
    МЕТА ДАННЫЕ: #5

--- apps/publishers-service/src/publishers-service.controller.ts
+++ apps/publishers-service/src/publishers-service.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
 
-import { CPublishersServiceService } from './publishers-service.service';
+import { ICreatePublisherInput, IPublisherService } from './publishers-service.interface';
 
 @Controller()
 export class CPublishersServiceController {
-  constructor(private readonly publishersServiceService: CPublishersServiceService) {}
+  constructor(
+    @Inject('CPublishersServiceService')
+    private readonly publishersServiceService: IPublisherService
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.publishersServiceService.getHello();
+  @GrpcMethod('CPublisherService', 'FindAll')
+  findAll() {
+    return { publishers: this.publishersServiceService.findAll() };
+  }
+
+  @GrpcMethod('CPublisherService', 'Create')
+  create(data: { createPublisherInput: ICreatePublisherInput }) {
+    return this.publishersServiceService.create(data.createPublisherInput);
+  }
+
+  @GrpcMethod('CPublisherService', 'FindOne')
+  findOne(data: { id: number }) {
+    return this.publishersServiceService.findOne(data.id);
   }
 }
