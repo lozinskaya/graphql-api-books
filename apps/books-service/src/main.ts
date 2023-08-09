737bddfcdab7d6b09ef118cce01e6b5f5c477a3a
natalalozinskaa <guvegina15N!>
2023-08-09 15:24:01 +0500

    feat(book): преобразовать books-service в микросервис
    МЕТА ДАННЫЕ: #6

--- apps/books-service/src/main.ts
+++ apps/books-service/src/main.ts
@@ -1,10 +1,21 @@
+import { join } from 'path';
+
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices/enums';
 
 import { CBooksServiceModule } from './books-service.module';
+const microserviceOptions = {
+  transport: Transport.GRPC,
+  options: {
+    url: `0.0.0.0:3003`,
+    package: 'books',
+    protoPath: join(process.cwd(), 'apps/books-service/src/books-service.proto'),
+  },
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(CBooksServiceModule);
+  const app = await NestFactory.createMicroservice(CBooksServiceModule, microserviceOptions);
 
-  await app.listen(3000);
+  await app.listen().then(() => console.log('Books service is listening...'));
 }
 bootstrap();
