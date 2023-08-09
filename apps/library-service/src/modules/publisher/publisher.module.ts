f7765d271617e7c1d4b50f7db6768ab5fc53dafc
natalalozinskaa <guvegina15N!>
2023-08-09 11:11:40 +0500

    refactor(publisher): прописать клиента publishers-service в library-service
    МЕТА ДАННЫЕ: #5

--- apps/library-service/src/modules/publisher/publisher.module.ts
+++ apps/library-service/src/modules/publisher/publisher.module.ts
@@ -1,10 +1,27 @@
+import { join } from 'path';
+
 import { Module } from '@nestjs/common';
+import { ClientProxyFactory, Transport, ClientGrpcProxy } from '@nestjs/microservices';
 
 import { CPublisherResolver } from './publisher.resolver';
-import { CPublisherService } from './publisher.service';
 
 @Module({
-  providers: [CPublisherResolver, CPublisherService],
-  exports: [CPublisherService],
+  providers: [
+    CPublisherResolver,
+    {
+      provide: 'PublishersServiceClient',
+      useFactory: (): ClientGrpcProxy => {
+        return ClientProxyFactory.create({
+          transport: Transport.GRPC,
+          options: {
+            url: `localhost:3002`,
+            package: 'publishers',
+            protoPath: join(process.cwd(), 'apps/library-service/src/modules/publisher/publisher.proto'),
+          },
+        });
+      },
+    },
+  ],
+  exports: ['PublishersServiceClient'],
 })
 export class CPublisherModule {}
