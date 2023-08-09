80b187dc7266e52a6b6716d7ddb5a86ef4596ec8
natalalozinskaa <guvegina15N!>
2023-08-09 11:25:02 +0500

    refactor(publisher): заменить сервис на микросервис
    Заменить сервис по издательствам в library-service на микросервис из publisher-service
    
    МЕТА ДАННЫЕ: #5

--- apps/library-service/src/modules/publisher/publisher.resolver.ts
+++ apps/library-service/src/modules/publisher/publisher.resolver.ts
@@ -1,24 +1,42 @@
+import { Inject, OnModuleInit } from '@nestjs/common';
 import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
+import { ClientGrpcProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 import { CCreatePublisherInput } from './dto/create-publisher.input';
-import { CPublisherService } from './publisher.service';
+import { IPublisherService } from './publisher.interface';
 
 @Resolver('Publisher')
-export class CPublisherResolver {
-  constructor(private readonly publisherService: CPublisherService) {}
+export class CPublisherResolver implements OnModuleInit {
+  constructor(
+    @Inject('PublishersServiceClient')
+    private readonly publisherClient: ClientGrpcProxy
+  ) {}
+
+  private publisherService: IPublisherService;
+
+  onModuleInit(): void {
+    this.publisherService = this.publisherClient.getService<IPublisherService>('CPublishersServiceService');
+  }
 
   @Mutation('createPublisher')
   createPublisher(@Args('createPublisherInput') createPublisherInput: CCreatePublisherInput) {
-    return this.publisherService.create(createPublisherInput);
+    return this.publisherService.create({ createPublisherInput });
   }
 
   @Query('publishers')
-  findPublishers() {
-    return this.publisherService.findAll();
+  async findPublishers() {
+    return (await lastValueFrom(this.publisherService.findAll({ data: null }))).publishers;
   }
 
   @Query('publisher')
-  findPublisher(@Args('id') id: number) {
-    return this.publisherService.findOne(id);
+  async findPublisher(@Args('id') id: number) {
+    try {
+      const publisher = await lastValueFrom(this.publisherService.findOne({ id }));
+
+      return publisher;
+    } catch (error) {
+      throw new Error('Издателя с id = ' + id + ' не найдена');
+    }
   }
 }
