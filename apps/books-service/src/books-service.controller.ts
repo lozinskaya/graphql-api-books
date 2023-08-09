f53ccd58ff3e8dd5ffa917951f0cb0fb6218b7cd
natalalozinskaa <guvegina15N!>
2023-08-09 15:28:10 +0500

    feat(book): описать контроллер для микросервиса books-service
    МЕТА ДАННЫЕ: #6

--- apps/books-service/src/books-service.controller.ts
+++ apps/books-service/src/books-service.controller.ts
@@ -1,13 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 
-import { CBooksServiceService } from './books-service.service';
+import { GrpcMethod } from '@nestjs/microservices';
+import { IBookService, ICreateBookInput } from './books-service.interface';
 
 @Controller()
 export class CBooksServiceController {
-  constructor(private readonly booksServiceService: CBooksServiceService) {}
 
-  @Get()
-  getHello(): string {
-    return this.booksServiceService.getHello();
+  constructor(
+    @Inject('CBooksServiceService')
+    private readonly bookService: IBookService
+  ) {}
+
+  @GrpcMethod('CBooksServiceService', 'FindAll')
+  findAll() {
+    return { books: this.bookService.findAll() };
   }
+
+  @GrpcMethod('CBooksServiceService', 'FindOne')
+  findOne(data: { id: number }) {
+    return this.bookService.findOne(data.id);
+  }
+
+  @GrpcMethod('CBooksServiceService', 'Create')
+  create(data: { createBookInput: ICreateBookInput }) {
+    return this.bookService.create(data.createBookInput);
+  }
+
+  @GrpcMethod('CBooksServiceService', 'FindByAuthorId')
+  findByAuthorId(data: { id: number }) {
+    return { books: this.bookService.findByAuthorId(data.id) };
+  }
+
+  @GrpcMethod('CBookService', 'FindByPublisherId')
+  findByPublisherId(data: { id: number }) {
+    return { books: this.bookService.findByPublisherId(data.id) };
+  }
+}
 }
